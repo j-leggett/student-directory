ca342522460561e1777d91d91e8e23b27e943fe5
Jack <jwleggett@outlook.com>
2023-03-06 20:18:29 +0000

    refactor with methods

--- directory.rb
+++ directory.rb
@@ -12,11 +12,25 @@ students = [
 "Joffrey Baratheon",
 "Norman Bates" ]
 
-# print student list using iteration
-puts "The student of Villains Academy"
-puts "------------"
-students.each do |student|
-  puts student
+
+def print_header
+  puts "The student of Villains Academy"
+  puts "------------"
 end
+# print student list using iteration
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
 # finally we print the total number of students
-puts "Overall, we have #{students.count} great students"
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+# nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
+
