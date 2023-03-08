bc557dcce0522fce48ade9dad50607c524eb01ae
Jack <jwleggett@outlook.com>
2023-03-08 16:41:19 +0000

    interacive menu

--- directory.rb
+++ directory.rb
@@ -32,9 +32,31 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
-# nothing happens until we call the methods
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def interactice_menu
+  students = []
+   # 4. repeat from step 1
+  loop do
+  # 1. print the menu and ask the user what to do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+  # 2. read the input and save it in to a variable
+  selection = gets.chomp
+  # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "i don't know what you meant, try again"
+    end
+  end
+end
 
+# nothing happens until we call the methods
+interactice_menu
